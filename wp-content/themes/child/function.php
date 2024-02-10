97d8a29839c8b47dd1913d76075682dad4a7a2e6
IhorRykun <IhorRykun@gmail.com>
2024-02-10 14:33:39 +0200

    update

--- wp-content/themes/child/function.php
+++ wp-content/themes/child/function.php
@@ -1,10 +1,11 @@
 <?php
-add_action('wp_enqueue_scripts', 'child_scripts');
+add_action('wp_enqueue_scripts', 'childhood_scripts');
 
-function child_scripts( ) {
-    wp_enqueue_style('child-style', get_stylesheet_uri() );
-
-    wp_enqueue_script('child-scripts', get_template_directory_uri() . '/assets/js/main.min.js', array(), null, true);
+function childhood_scripts() {
+    wp_enqueue_style( 'childhood-style', get_stylesheet_uri( ) );
+    wp_enqueue_style( 'style-header', get_template_directory_uri( ) . "assets/style/header.css" );
+    wp_enqueue_script( 'childhood-scripts', get_template_directory_uri( ) . 'assets/js/main.min.js', array(), null, true);
+    wp_enqueue_script('jquery_my', 'https://code.jquery.com/jquery-3.0.0.min.js');
 };
 
-?>
+?>  
