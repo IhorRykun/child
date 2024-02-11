8816084d01c57928a731f36e686e844e19aa6be5
IhorRykun <IhorRykun@gmail.com>
2024-02-11 19:18:32 +0200

    update files

--- childhood/function.php
+++ childhood/function.php
@@ -1,17 +1,10 @@
 <?php
-add_action('wp_enqueue_scripts', 'childhood_style');
-add_action('wp_enqueue_scripts', 'childhood_script');
+add_action('wp_enqueue_scripts', 'childhood_scripts');
 
-function childhood_script() {
- wp_enqueue_script('childhood-scripts', get_template_directory_uri(  ) . '/js/main.min.js', array(), null, true);
+function childhood_scripts() {
+    wp_enqueue_style( 'childhood-style', get_stylesheet_uri() );
+
+    wp_enqueue_script( 'childhood-scripts', get_template_directory_uri() . '/assets/js/main.min.js', array(), null, true);
 };
 
-
-function childhood_style() {
- wp_enqueue_style('childhood-style', get_stylesheet_uri() );
- wp_enqueue_style('style-header', get_stylesheet_directory() . 'assets/style/header.css' );
-}
-;
 ?>
-
-
