e9da2ae7f953431892734b497f47f4158b6331e8
IhorRykun <IhorRykun@gmail.com>
2024-03-05 20:56:15 +0200

    add review

--- wp-content/themes/child/functions.php
+++ wp-content/themes/child/functions.php
@@ -2,10 +2,18 @@
 add_action('wp_enqueue_scripts', 'childhood_scripts');
 
 function childhood_scripts() {
+
+ // Підключення стилів глоабально
     wp_enqueue_style( 'childhood-style', get_stylesheet_uri() );
 
+    // Підключення стилів та js файлів
     wp_enqueue_script( 'childhood-scripts', get_template_directory_uri() . '/assets/js/main.min.js', array(), null, true);
    };
-   
-   add_theme_support("custom-logo")
+
+   // Дає можливість додавати лого з адмінки
+add_theme_support("custom-logo");
+
+
+// Дає можливість додавати привю зображень
+      add_theme_support("post-thumbnails");
 ?>
